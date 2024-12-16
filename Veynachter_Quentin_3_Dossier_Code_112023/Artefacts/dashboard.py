aba202cab48f005145a13cbfb20a6a4ca5dd1144
Quentin VEYNACHTER <131259431+QVEYNACHTER@users.noreply.github.com>
2024-12-16 17:41:16 +0100

    Retouches finales dashboard.py

--- Veynachter_Quentin_3_Dossier_Code_112023/Artefacts/dashboard.py
+++ Veynachter_Quentin_3_Dossier_Code_112023/Artefacts/dashboard.py
@@ -58,21 +58,23 @@ st.markdown("""
 
 if st.button('Run') or state['data_received']:
     #On vérifie si l'ID actuel est différent du dernier ID appelé
-    if state['data_received']:
+    if state['data_received'] and state['last_sk_id_curr'] == sk_id_curr:
         st.success('Données déjà reçues pour ce client')
-        
+
+    #Si l'ID a changé on réinitialise les données
     if state['last_sk_id_curr'] != sk_id_curr:
         state['data_received'] = False
         state['last_sk_id_curr'] = sk_id_curr  #On met à jour le dernier ID
 
+    #Si les données n'ont pas été reçues pour ce client
     if not state['data_received']:
         response = requests.post('http://localhost:5000/predict', json={'SK_ID_CURR': int(sk_id_curr)})
         if response.status_code != 200: #Erreur 200 indique que la requête a été traitée avec succès
             st.error(f"Erreur lors de l'appel à l'API : {response.status_code} - {response.text}")
             st.stop()
 
-        state['data'] = response.json()
-        state['data_received'] = True
+        state['data'] = response.json() #Sauvegarde les données reçues
+        state['data_received'] = True #Marque les données comme reçues
 
     data = state['data']
 
@@ -91,7 +93,7 @@ if st.button('Run') or state['data_received']:
     #On applique la couleur en fonction de la proba
     color = compute_color(proba)
 
-    #Création d'une jaune pour voir où se positionne le client par rapport au threshold
+    #Création d'une jauge pour voir où se positionne le client par rapport au threshold
     jauge_html = f"""
                 <div style="position: relative; width: 100%; height: 30px; background: linear-gradient(to right, green {threshold}%, red {threshold}%); border-radius: 15px">
                     <div style="position: absolute; top: 50%; left: {proba}%; transform: translateX(-50%) translateY(-50%);
@@ -102,9 +104,15 @@ if st.button('Run') or state['data_received']:
     #On affiche
     st.markdown(jauge_html, unsafe_allow_html=True)
 
+    #Un peu d'espace avant la suite
+    st.markdown("<br>", unsafe_allow_html=True)
+
     #Message en fonction de la décision
     message_decision = ('Risque faible' if proba < threshold else 'Risque potentiel')
     st.markdown(f"<div style='text-align: center; color:{color}; font-size:30px; border:2px solid {color}; padding:10px;'>{message_decision}</div>", unsafe_allow_html=True)
+
+    #Encore un espace
+    st.markdown("<br>", unsafe_allow_html=True)
 
     #On filtre le top 10 features qui réduisent et augmentent le risque
     shap_df_decrease = shap_df[shap_df['SHAP Value'] < 0].sort_values(by='SHAP Value').head(10)
@@ -127,16 +135,15 @@ if st.button('Run') or state['data_received']:
 
     #On ajoute les feature values en annotations
     for bar, feature_value in zip(bars_left, shap_df_decrease['Feature Value']):
-        axes[0].text(bar.get_width() + 0.01, #Dans la barre
+        axes[0].text(bar.get_width() - 0.005, #Hors de la barre
                      bar.get_y() + bar.get_height() / 2, #Centré dans la barre
                      f'{feature_value}',
-                     va='center', ha='right', fontsize=10, fontweight='bold', color='black') #Aligné à droite
+                     va='center', ha='left', fontsize=10, fontweight='bold', color='black') #Aligné à gauche
 
     #Graphique pour les features qui augmentent le risque
     bars_right = axes[1].barh(shap_df_increase['Feature'], shap_df_increase['SHAP Value'], color='lightcoral')
     axes[1].set_xlabel('Feature importance')
     axes[1].set_title('Top 10 features qui augmentent le risque', weight='bold')
-    axes[1].invert_yaxis() #Inverse l'axe pour que la feature la plus importante soit en haut
     axes[1].invert_xaxis() #Inverse l'axe pour les barres pointent vers la gauche
     axes[1].yaxis.tick_right() #Déplace les yticks à droite
     axes[1].yaxis.set_tick_params(left=False) #Désactive les yticks à gauche
@@ -148,9 +155,9 @@ if st.button('Run') or state['data_received']:
 
     #On ajoute les feature values en annotations
     for bar, feature_value in zip(bars_right, shap_df_increase['Feature Value']):
-        axes[1].text(bar.get_width() - 0.01, #Dans la barre
+        axes[1].text(bar.get_width() + 0.005, #Hors de la barre
                      bar.get_y() + bar.get_height() / 2, #Centré dans la barre
                      f'{feature_value}',
-                     va='center', ha='left', fontsize=10, fontweight='bold', color='black') #Aligné à gauche
+                     va='center', ha='right', fontsize=10, fontweight='bold', color='black') #Aligné à droite
 
     st.pyplot(fig)
