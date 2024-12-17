1b5f34eb082240c43c9ef76a19e50bd280437c6f
Quentin VEYNACHTER <131259431+QVEYNACHTER@users.noreply.github.com>
2024-12-17 10:16:41 +0100

    Changement PORT pour Azure

--- Veynachter_Quentin_1_API_112023.py
+++ Veynachter_Quentin_1_API_112023.py
@@ -7,6 +7,10 @@ dir = './Veynachter_Quentin_3_Dossier_Code_112023/Artefacts'
 #On récupère les variables d'environnement
 env = os.environ.copy()
 
+#Azure définit le port dans la variable d'environnement "PORT"
+#Ici on utilise donc soit le port imposé par Azure, soit le port par défaut (en local)
+port = env.get('PORT', '8501')
+
 #On exécute api.py et dashboard.py
 subprocess.Popen(['python', f'{dir}/api.py'], env=env)
-subprocess.Popen(['streamlit', 'run', f'{dir}/dashboard.py', '--server.headless=true'], env=env)
+subprocess.Popen(['streamlit', 'run', f'{dir}/dashboard.py', '--server.headless=true', f'--server.port={port}', '--server.address=0.0.0.0'], env=env)
