544d4ac541f2ba008b85d66befaab49e59ad1656
Quentin VEYNACHTER <131259431+QVEYNACHTER@users.noreply.github.com>
2025-01-03 17:22:39 +0100

    Forcage de PORT

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ dir = './Veynachter_Quentin_3_Dossier_Code_112023/Artefacts'
 #On récupère les variables d'environnement
 env = os.environ.copy()
 
-#Azure définit le port dans la variable d'environnement "PORT"
-#Ici on utilise donc soit le port imposé par Azure, soit le port par défaut (en local)
-port = env.get('PORT', '8501')
+#On modifie la variable d'environnement PORT
+env['PORT'] = '5000'
 
 #On exécute api.py et dashboard.py
 subprocess.Popen(['python', f'{dir}/api.py'], env=env)
-subprocess.Popen(['streamlit', 'run', f'{dir}/dashboard.py', '--server.headless=true', f'--server.port={port}', '--server.address=0.0.0.0'], env=env)
+subprocess.Popen(['streamlit', 'run', f'{dir}/dashboard.py', '--server.headless=true', '--server.port=8000', '--server.address=0.0.0.0'], env=env)
